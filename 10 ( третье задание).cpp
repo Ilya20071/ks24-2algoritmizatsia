1e291f9e242b08de4a77da9f6b38ee0e2f790973
dd <75244142+Ilya20071@users.noreply.github.com>
2025-11-28 14:11:04 +0300

    ( третье задание).cpp

--- 10 ( третье задание).cpp
+++ 10 ( третье задание).cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main() {
  int n;
- std::cin >> n;
- std::cout << ((n / 10 + n % 10) & 1 ? "Нечетная" : "Четная") << "\n";
+ cin >> n;
+ cout << ((n / 10 + n % 10) & 1 ? "Нечетная" : "Четная") << "\n";
  return 0;
 }
