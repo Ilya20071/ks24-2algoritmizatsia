66c9337a40a9a22106dc8db057d6acd23743392a
dd <75244142+Ilya20071@users.noreply.github.com>
2025-11-28 13:09:46 +0300

    1 первое задание).cpp

--- 19(1 первое задание).cpp
+++ 19(1 первое задание).cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
    double a, b, c;
    
-   // Ввод значений a b c
+  
    std::cout << "Введите значение a:";
    std::cin >> a;
    std::cout << "Введите значение b: ";
@@ -14,10 +14,10 @@ int main() {
    std::cout >> "Введите значение c:";
    std::cin >> c;
    
-   // Вычисление выражения
+   
    double result = (b + sqrt(b*b + 4*a*c)) / (2*a) - pow(a, 3) + pow(b, -2);
    
-   // Вывод результата
+  
    std::cout << "Результат:" << result << std::endl;
    return 0;
 }
