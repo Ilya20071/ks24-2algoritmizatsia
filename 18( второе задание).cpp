b6fefefd8e57df28a68c113aead33c29582e14ec
dd <75244142+Ilya20071@users.noreply.github.com>
2025-11-28 12:47:13 +0300

    Add files via upload

---
+++ 18( второе задание).cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// Функция вычисления радиуса описанной окружности
+double radiusOfCircumscribedCircle(double a) {
+    return a / sqrt(3);
+}
+
+int main() {
+    double sideLength = 10.0; // Длина стороны треугольника 
+    
+    cout << "Длина стороны равностороннего треугольника: " << sideLength << endl;
+    cout << "Радиус описанной окружности: " << radiusOfCircumscribedCircle(sideLength) << endl;
+    
+    return 0;
+}
